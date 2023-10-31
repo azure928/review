a395eda7e08a3e60cec5b1aff9ce35a91e667e32
Ji Yook <azure6.dev@gmail.com>
2023-10-31 23:58:46 +0900

    리뷰 등록, 삭제 API

---
+++ src/main/java/com/ji/review/repository/ReviewRepository.java
@@ -0,0 +1,7 @@
+package com.ji.review.repository;
+
+import com.ji.review.model.ReviewEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+}
